87f5e760011ae0657e800210fe2d21dfa69b95ff
Renato <renatodev95@gmail.com>
2021-08-16 09:40:18 -0300

    Correcao na distruicao das minas

--- campo-minado/src/br/com/cod3r/cm/visao/TabuleiroConsole.java
+++ campo-minado/src/br/com/cod3r/cm/visao/TabuleiroConsole.java
@@ -69,8 +69,10 @@ public class TabuleiroConsole {
 				}
 			}
 
+			System.out.println(tabuleiro);
 			System.out.println("Você ganhou!!!");
 		} catch (ExplosaoException e) {
+			System.out.println(tabuleiro);
 			System.out.println("Você perdeu!");
 		}
 	}
