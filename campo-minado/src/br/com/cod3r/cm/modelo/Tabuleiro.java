5780ffc9cdfbcfbb2ded6296deadcf1ebab6f54f
Renato <renatodev95@gmail.com>
2021-08-16 08:26:17 -0300

    Implementacao de metodos finais

--- campo-minado/src/br/com/cod3r/cm/modelo/Tabuleiro.java
+++ campo-minado/src/br/com/cod3r/cm/modelo/Tabuleiro.java
@@ -2,6 +2,7 @@ package br.com.cod3r.cm.modelo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Tabuleiro {
 
@@ -31,14 +32,33 @@ public class Tabuleiro {
 
 	private void associarVizinhos() {
 		for (Campo c1 : campos) {
-			for (Campo c2: campos) {
+			for (Campo c2 : campos) {
 				c1.adicionarVizinho(c2);
 			}
 		}
 	}
 
 	private void sortearMinas() {
-
+		long minasArmadas = 0;
+		Predicate<Campo> minado = campo -> campo.isMinado();
+		
+		do {
+			minasArmadas = campos.stream().filter(minado).count();
+			int aleatorio = (int) (Math.random() * campos.size());
+			campos.get(aleatorio).minar();
+		} while (minasArmadas < minas);
 	}
 
+	boolean objetivoAlcancado() {
+		return campos.stream().allMatch(c -> c.objetivoAlcancado());
+	}
+	
+	public void reiniciar() {
+		campos.stream().forEach(c -> c.reiniciar());
+		sortearMinas();
+	}
+	
+	public String toString() {
+		return null;
+	}
 }
