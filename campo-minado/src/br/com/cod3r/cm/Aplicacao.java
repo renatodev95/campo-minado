c81ea78ecfaa787b340ddbe3f245673f2c8a205e
Renato <renatodev95@gmail.com>
2021-08-16 09:40:30 -0300

    Correcao na distribuicao de minas

--- campo-minado/src/br/com/cod3r/cm/Aplicacao.java
+++ campo-minado/src/br/com/cod3r/cm/Aplicacao.java
@@ -6,10 +6,9 @@ import br.com.cod3r.cm.visao.TabuleiroConsole;
 public class Aplicacao {
 
 	public static void main(String[] args) {
-		
-		Tabuleiro tabuleiro = new Tabuleiro(6,6, 6);	
+
+		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 3);
 		new TabuleiroConsole(tabuleiro);
-		
-		
+
 	}
 }
